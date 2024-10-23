2423cf71955b5f93f9035b3ff9d4d3b3bbdfe6a6
Ketaklak <ketaklak33@gmail.com>
2024-10-24 01:26:01 +0200

    Commentaires enlevé

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -1,4 +1,4 @@
-// Fonction pour générer une couleur aléatoire
+
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -8,8 +8,8 @@ function getRandomColor() {
     return color;
 }
 
-// Sélectionne le bouton et ajoute un événement 'click'
+
 document.getElementById('colorButton').addEventListener('click', function() {
-    // Change la couleur de fond de la page
+
     document.body.style.backgroundColor = getRandomColor();
 });
